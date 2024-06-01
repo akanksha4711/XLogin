ad89daa6d1a67aa30223ae8b22c41e9abd304923
Akanksha Sharma <akku.sharma97@gmail.com>
2024-06-01 21:53:44 +0530

    completed app

--- xlogin/src/App.js
+++ xlogin/src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const username = 'username';
+  const password = 'password';
+  const [invalid, setInvalid] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
+  if (loggedIn){
+    return (
+      <div className='App'>
+        <h1>Login Page</h1>
+        <div>Welcome, User!</div>
+      </div>
+    )
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Login Page</h1>
+      {invalid && <p>Invalid username or password</p>}
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        if(password !== e.target.password.value){
+          setInvalid(true);
+        }
+        if(username !== e.target.username.value){
+          setInvalid(true);
+        }
+        if(password === e.target.password.value && username === e.target.username.value){
+          setLoggedIn(true);
+        }
+      }}>
+        <label name="username">Username: </label>
+        <input name="username" type="text"/>
+        <br/>
+        <label name="password">Password: </label>
+        <input name="password" type="password"/>
+        <br/>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
